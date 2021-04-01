2175701b97cd4e7360268e2099df8be3563e5666
Caoilainn McCrory <caoilainnmccrory@gmail.com>
2021-04-01 14:22:12 +0100

    Commit message

--- app/src/main/java/com/example/virtualwallet/CloudAgent.java
+++
@@ -1,7 +0,0 @@
-package com.example.virtualwallet;
-
-import com.google.gson.annotations.SerializedName;
-
-public class CloudAgent {
-    @SerializedName("cloud_agent_id") public String cloudAgentId;
-}
