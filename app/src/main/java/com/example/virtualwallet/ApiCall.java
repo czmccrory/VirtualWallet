2e69fa9bc0fc3f39f1d4e865ad9b5ba541537b25
Caoilainn McCrory <caoilainnmccrory@gmail.com>
2021-04-01 14:31:37 +0100

    Added lost files

---
+++ app/src/main/java/com/example/virtualwallet/ApiCall.java
@@ -0,0 +1,11 @@
+package com.example.virtualwallet;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface ApiCall {
+    @POST("cloudagents")
+    Call<CloudAgent> Register(@Body Registration body);
+
+}
